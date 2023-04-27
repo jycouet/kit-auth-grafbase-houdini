ae7298139452d692fa0e5c7df1860384cae3784c
jycouet <jycouet@gmail.com>
2023-04-27 09:50:29 +0200

    :sparkles: NEW: routes (public & protected)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>kit-auth-grafbase-houdini</title>
+</svelte:head>
+
+<h1>kit-auth-grafbase-houdini</h1>
+
+<hr />
+
+<nav>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/pricing">Pricing</a></li>
+		<li><a href="/dashboard">Dashboard</a></li>
+	</ul>
+	<ul>
+		<li><a href="/#">Login</a></li>
+		<li><a href="/me">MY NAME</a></li>
+	</ul>
+</nav>
+
+<hr />
+
+<slot />
+
+<style>
+	nav {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	ul {
+		display: flex;
+		gap: 1rem;
+
+		list-style: none;
+		margin-block: 0;
+		padding-inline: 0;
+	}
+</style>
